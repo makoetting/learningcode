76c05a4f44a49743c4113da9dc475a8cf610fd05
Michael Koetting <makoetting@users.noreply.github.com>
2014-08-10 12:20:23 -0500

    ruby hard way excerceis 1-10

---
+++ ruby_hardway/ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}"
+puts "Rooster #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #in floating point, would need to include decimal points for 1/4 to equal .25 e.g. 1.0/4.0 = .25 vs 1/4 = 0
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's fales."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ ruby_hardway/ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Michael Koetting'
+my_age = 24 #based in year 2014
+my_height = 12 * 6 #inches
+my_weight = 165 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Sandy'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's #{my_weight} light."
+puts "He has #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are #{my_teeth}."
+
+#this line is tricky
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
